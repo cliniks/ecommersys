a0b1b04793e101c68bfdb4b4dd8e29b8442e9f32
Victor Augusto dos Santos <ainomichi@hotmail.com>
2022-12-15 12:41:53 -0300

    return user from token updated

--- apps/api-eCommerce/src/utils/returnUserFromToken.ts
+++ apps/api-eCommerce/src/utils/returnUserFromToken.ts
@@ -7,16 +7,12 @@ const returnUserFromToken = async (req: Request): Promise<User> => {
   try {
     const repo = new UsersRepository();
     const token = req.headers["x-access-token"];
-    console.log({ token });
     const decoded: User | any = jwt.decode(`${token}`);
-    console.log({ decoded });
     const userID = decoded._id;
-    console.log({ userID });
     const user = (await repo.getOne({
       key: "_id",
       value: userID,
     })) as User;
-    console.log({ user });
     return user;
   } catch (error) {
     throw new Error("Não foi possível achar um usuário a partir desse token!");
